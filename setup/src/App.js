3021eb8e4de054b9a2f8a15eb73b3a98666b44d9
DouglasGo8 <sgtear@hotmail.com>
2021-04-16 08:38:30 -0300

    Basic Setup Done

---
+++ setup/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+const app = () => {
+  return (
+    <div>
+      <hr />
+      Content
+      <hr />
+    </div>
+  );
+};
+
+export default app;
