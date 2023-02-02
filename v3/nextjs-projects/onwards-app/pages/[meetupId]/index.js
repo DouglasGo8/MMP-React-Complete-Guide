c40de219079ffa10a164a51dc00c43ed5c64cfb7
DouglasGo8 <abraham.dias.douglas@gmail.com>
2023-02-02 17:37:08 -0300

    End of React.js

--- v3/nextjs-projects/onwards-app/pages/[meetupId]/index.js
+++ v3/nextjs-projects/onwards-app/pages/[meetupId]/index.js
@@ -1,2 +1,7 @@
+import MeetupDetails from "../../components/meetups/MeetupDetail";
 
+const MeetupDetails = () => {
+  return <MeetupDetails image="" title="" address="" description="" />;
+};
 
+export default MeetupDetails();
